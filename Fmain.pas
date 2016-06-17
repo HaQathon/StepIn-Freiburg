22915f62cf28f54247a7fc3532539b8835ca413a
Niklas Ritzenhoff <niklas.ritzenhoff@gmail.com>
2016-06-17 20:53:33 +0200

    Diverse

--- Fmain.pas
+++ Fmain.pas
@@ -7,7 +7,7 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.ListView, FMX.MultiView, FMX.Edit,
-  FMX.Objects;
+  FMX.Objects, FMX.Header;
 
 type
   TForm2 = class(TForm)
@@ -16,6 +16,14 @@ type
     Edit1: TEdit;
     ListView2: TListView;
     Image1: TImage;
+    Header1: THeader;
+    lbl_title: TLabel;
+    btn_OpenMultiView: TButton;
+    head_Mview: THeader;
+    Button1: TButton;
+    lbl_MultiView: TLabel;
+    procedure btn_OpenMultiviewClick(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
     { Private-Deklarationen }
   public
@@ -30,5 +38,16 @@ implementation
 {$R *.fmx}
 {$R *.LgXhdpiPh.fmx ANDROID}
 {$R *.Surface.fmx MSWINDOWS}
+
+procedure TForm2.btn_OpenMultiviewClick(Sender: TObject);
+ begin
+  MultiView1.ShowMaster
+ end;
+
+
+procedure TForm2.Button1Click(Sender: TObject);
+ begin
+  MultiView1.HideMaster;
+ end;
 
 end.
