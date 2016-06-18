9daad603ec142a70a6d5fbacabb81fd7ae489be0
Niklas Ritzenhoff <niklas.ritzenhoff@gmail.com>
2016-06-18 08:45:39 +0200

    Restore von Fmain

--- Fmain.pas
+++ Fmain.pas
@@ -7,7 +7,7 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
   FMX.Controls.Presentation, FMX.StdCtrls, FMX.ListView, FMX.MultiView, FMX.Edit,
-  FMX.Objects;
+  FMX.Objects, FMX.Header;
 
 type
   TForm2 = class(TForm)
@@ -16,8 +16,13 @@ type
     Edit1: TEdit;
     ListView2: TListView;
     Image1: TImage;
+    Header1: THeader;
+    lbl_title: TLabel;
+    btn_OpenMultiView: TButton;
+    head_Mview: THeader;
     Button1: TButton;
-    Label1: TLabel;
+    Aufgaben: TLabel;
+    procedure btn_OpenMultiviewClick(Sender: TObject);
     procedure Button1Click(Sender: TObject);
   private
     { Private-Deklarationen }
@@ -31,14 +36,19 @@ var
 implementation
 
 {$R *.fmx}
-uses
-  uTask;
+{$R *.LgXhdpiPh.fmx ANDROID}
+{$R *.Surface.fmx MSWINDOWS}
+
+procedure TForm2.btn_OpenMultiviewClick(Sender: TObject);
+ begin
+  MultiView1.ShowMaster;
+ end;
+
 
 procedure TForm2.Button1Click(Sender: TObject);
-var task: TTask;
-  begin
-   task := TaskDatenbank.get(1);
-   Label1.Text := task.taskInfo;
-  end;
+ begin
+  MultiView1.HideMaster;
+
+ end;
 
 end.
